094e0c804741586923269bc559c15fe8e6c10e63
gwilliamboyd <gwillboyd@gmail.com>
2025-04-23 19:34:29 -0500

    Memoize users and handle errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,34 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [users, setUsers] = useState([])
+// memoize users
+const UsersList = React.memo(({ users }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.id}>
+        {user.username} - {user.email}
+      </li>
+    ))}
+  </ul>
+))
 
+// component
+function App() {
+  // users state
+  const [users, setUsers] = useState([])
+  const [hasError, setHasError] = useState(false)
+
+  // fetch users on mount
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const response = await fetch('http://localhost:8000/users')
         const users = await response.json()
-        console.log(users)
         setUsers(users)
         return users
       } catch (err) {
         console.error('Error fetching users: ', err)
+        setHasError(true)
       }
     }
     fetchUsers()
@@ -22,11 +37,7 @@ function App() {
   return (
     <>
       <h1>Users</h1>
-      {users.map(user => (
-        <li key={user.id}>
-          {user.username} - {user.email}
-        </li>
-      ))}
+      {!hasError ? <UsersList users={users} /> : <h2>Error loading users.</h2>}
     </>
   )
 }
